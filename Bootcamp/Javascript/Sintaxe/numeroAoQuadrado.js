c4dab6e88fdf450e60e9a1a2b8b71bca51cd6603
dmenezes <148342102+ogdie@users.noreply.github.com>
2025-09-17 12:32:58 +0100

    Add files via upload

---
+++ Bootcamp/Javascript/Sintaxe/numeroAoQuadrado.js
@@ -0,0 +1,9 @@
+/*
+Encontra os erros de sintaxe do seguinte excerto.
+
+A função numeroAoQuadrado recebe um número e retorna o quadrado desse número.
+*/
+
+function numeroAoQuadrado(num) {
+    return num * num;
+}
