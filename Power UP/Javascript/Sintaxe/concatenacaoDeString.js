5d6087955a375bebfbc443ff87407b56459e682b
dmenezes <148342102+ogdie@users.noreply.github.com>
2025-09-17 12:30:32 +0100

    Add files via upload

---
+++ Power UP/Javascript/Sintaxe/concatenacaoDeString.js
@@ -0,0 +1,8 @@
+/* 
+Faz a concatenação de duas strings que se encontram nas variáveis a e b,
+deixando um espaço entre elas e atribui o resultado à variável c.
+*/
+
+let a = "Olá";
+let b = "Mundo";
+let c = a + " " + b;
